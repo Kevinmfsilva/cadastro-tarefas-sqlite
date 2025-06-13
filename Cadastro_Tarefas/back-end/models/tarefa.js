b2fc43a2987680e47176d3bd7d3eb263f9dcd681
Kevinmfsilva <166409636+Kevinmfsilva@users.noreply.github.com>
2025-06-13 12:35:47 -0300

    Update tarefa.js

---
+++ Cadastro_Tarefas/back-end/models/tarefa.js
@@ -0,0 +1,24 @@
+onst{DataTypes}=require("sequelize");
+const sequelize = require ("../database/db");
+
+const Tarefa = sequelize.define("Tarefa", {
+    titulo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+ 
+    },
+    descricao: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    responsavel:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    status:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+
+
+});
